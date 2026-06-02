7e2ca3a76898caa0c35a7efb87e9d3b9c7a77623
Thando Mthembu <thandomthembu54321@gmail.com>
2026-06-02 14:21:12 +0200

    Added getHumanChoices function
    Created a function to get user input  by typing one of the choices of rock, paper, or scissors

--- javascript.js
+++ javascript.js
@@ -1,7 +1,21 @@
+//Write a function to get the computer's choice
 const getComputerChoice = function(){
+    // The choices will be rock, paper, or scissors. We will use an array to store these choices and then randomly select one of them.
    const choices = ["rock", "paper", "scissors"];
+   // We will use the Math.random() function to generate a random number between 0 and 1, and then multiply it by the length of the choices array to get a random index. We will then use this index to return the computer's choice.
    const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
-    
+};
 
-}
+//Write a function to get the human choice
+const getHumanChoice = function(){
+    // Use the prompt method to get user's input
+    const userInput = prompt("Please enter rock, paper, or scissors: ");
+    //Convert the user's input to lowercase to make it case-insensitive
+    return userInput.toLowerCase();
+    //User must type rock, paper, or scissors. If they type something else, we will return an error message.
+    if(userInput !== "rock" && userInput !== "paper" && userInput !== "scissors"){
+        return "Invalid choice. Please enter rock, paper, or scissors.";
+    }
+
+};
