3f2c766a56e083effb32915ff4970c2db49450e1
Thando Mthembu <thandomthembu54321@gmail.com>
2026-06-08 20:48:32 +0200

    Get Human choice function
    made the choice case sensitive

--- javascript.js
+++ javascript.js
@@ -10,54 +10,46 @@ const getComputerChoice = function(){
 //Write a function to get the human choice
 const getHumanChoice = function(){
     // Use the prompt method to get user's input
-    const userInput = prompt("Please enter rock, paper, or scissors: ");
+    const choice = prompt("Please enter rock, paper, or scissors: ").toLowerCase();
         //User must type rock, paper, or scissors. If they type something else, we will return an error message.
-    if(userInput !== "rock" && userInput !== "paper" && userInput !== "scissors"){
-        return "Invalid choice. Please enter rock, paper, or scissors.";
+    while(choice !== "rock" && choice !== "paper" && choice !== "scissors"){
+        choice = prompt("Invalid choice. Please enter rock, paper, or scissors: ").toLowerCase();
     }
-
+    return choice;
 };
-
-
-//Create a function with two arguments, humanChoice and computerChoice
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-
 //Create a function named playGame
 const playGame = function(){
     // Move playRound function and score variables so that they're declared inside of the new playGame function. This way, the scores will reset every time the playGame function is called.
-    const playRound = function (humanChoice, computerChoice){
-    // The humanChoice argument must be case-insensitive
-        humanChoice = humanChoice.toLowerCase();
-    // The console.log must display a string value representing the round winner, such as: 'You lose! paper bears Rock"
-    //Increment the humanScore or computerScore variable based on the round winner 
-    if (humanChoice === computerChoice){
-        return "It's a tie! You both chose " + humanChoice;
-    } else if (humanChoice === "rock" && computerChoice === "scissors"){
-        humanScore++;
-        return "You win! Rock beats scissors";
-    } else if (humanChoice === "paper" && computerChoice === "rock"){
-        humanScore++;
-        return "You win! Paper beats rock";
-    } else if (humanChoice === "scissors" && computerChoice === "paper"){
-        humanScore++;
-        return "You win! Scissors beats paper";
-    } else {
-        computerScore++;
-        return "You lose! " + computerChoice + " beats" + humanChoice;
-    }    
-    };
-    // Create a variable named humanScore  and set it to zero
+     // Create a variable named humanScore  and set it to zero
     let humanScore = 0;
     // Create a variable named computerScore and set it to zero
     let computerScore = 0;
-};
+    const playRound = function (humanChoice, computerChoice){
+         // The humanChoice argument must be case-insensitive, so we will convert it to lowercase using the toLowerCase() method.
+         // The console.log must display a string value representing the round winner, such as: 'You lose! paper bears Rock"
+         //Increment the humanScore or computerScore variable based on the round winner 
+       if (humanChoice === computerChoice){
+          return "It's a tie! You both chose " + humanChoice;
+       } else if (humanChoice === "rock" && computerChoice === "scissors"){
+          humanScore++;
+          return "You win! Rock beats scissors";
+       } else if (humanChoice === "paper" && computerChoice === "rock"){
+          humanScore++;
+          return "You win! Paper beats rock";
+       } else if (humanChoice === "scissors" && computerChoice === "paper"){
+          humanScore++;
+          return "You win! Scissors beats paper";
+       } else {
+          computerScore++;
+          return "You lose! " + computerChoice + " beats" + humanChoice;
+       }    
+    };
     // Play five rounds calling playRound 5 times
     for (let i = 0; i < 5; i++){
         const humanSelection = getHumanChoice();
         const computerSelection = getComputerChoice();
-        console.log(playRound(humanSelection, computerSelection))
+        console.log(playRound(humanSelection, computerSelection));
     }
+};
+
+  playGame();
